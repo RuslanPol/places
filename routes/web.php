d6751aea7950f7876602a801bc573df6e9e5f35e
RuslanPol <vvrv@yandex.ru>
2025-01-20 20:24:27 +0700

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome to laravel');
+    return view('welcome');
 });
 
 use App\Http\Controllers\PlaceController;
